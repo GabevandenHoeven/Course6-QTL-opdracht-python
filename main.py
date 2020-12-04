d6f6916e8bdce528d60467012f61c92adf1a8f5b
MaxNollet <mjh.nollet@student.han.nl>
2020-12-04 15:25:56 +0100

    Banner toegevoegd en bestandsnamen alvast in de main-functie gezet.

--- main.py
+++ main.py
@@ -3,7 +3,17 @@
 def main():
 
     string = "Hallo"
+
+    locBestand = "CvixLerC9.loc"
+    quaBestand = "CvixLerC9.qua"
     print(string)
+    return 0
 
 
-main()
+if __name__ == "__main__":
+    print("/---------------------------------\\")
+    print("|    QTL-opdracht assignment 2    |")
+    print("|         Ontwikkeld door:        |")
+    print("| Gabe van de Hoeven & Max Nollet |")
+    print("\---------------------------------/\n")
+    main()
