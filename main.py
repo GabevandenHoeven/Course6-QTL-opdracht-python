821ad670294ac341640f5fd29b7e96b2a7019535
GabevandenHoeven <gabevandenhoeve34@gmail.com>
2020-12-05 12:46:15 +0100

    read_file function aangemaakt die de bestanden kan inlezen en 2 dictionaries maakt

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 
+def read_files(loc, qua):
+    """
+
+    :param loc:
+    :param qua:
+    :return:
+    """
+    d = {}
+    d2 = {}
+    with open(loc, "r") as loc_bes:
+        loc_line = ""
+        for line in loc_bes:
+            if " (a,b) ;" in line:
+                start_reading = True
+                if not loc_line == "":
+                    d.update({header: loc_line})
+                header = line.split(" (a,b) ;")[0]
+            elif "individual names:" == line:
+                start_reading = False
+                d.update({header: loc_line})
+            elif start_reading:
+                loc_line += line
+        loc_bes.close()
+
+    with open(qua, "r") as qua_bes:
+
+        for line in qua_bes:
+            if line.startswith("1"):
+                start_reading = True
+            elif line == "":
+                start_reading = False
+            elif start_reading:
+                index, num = line.split("\t")
+                d2.update({index: num})
+        qua_bes.close()
+
+    return
+
 
 def main():
 
-    string = "Hallo"
+    loc_bestand = "CvixLerC9.loc"
+    qua_bestand = "CvixLerC9.qua"
 
-    locBestand = "CvixLerC9.loc"
-    quaBestand = "CvixLerC9.qua"
-    print(string)
+    read_files(loc_bestand, qua_bestand)
     return 0
 
 
