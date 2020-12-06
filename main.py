8444bc2c54f39e7a7643d3d3677798cdc97291fb
GabevandenHoeven <gabevandenhoeve34@gmail.com>
2020-12-06 16:48:55 +0100

    Readfile functie afgeschreven returnt nu 2 dictionaries: 1e voor loc file met keys gen names en values lijst met de band dingen. 2e voor qua file met keys de positie en values de kleine getallen. LET OP: DE VALUES IN DE 2E DICTIONARY ZIJN STRINGS!

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 
 def read_files(loc, qua):
-    """
+    """ This function reads a loc and a qua file and puts the contents in dictionaries
 
-    :param loc:
-    :param qua:
-    :return:
+    :param loc: string - The filename of the loc file
+    :param qua: string - The filename of the qua file
+
+    :return d: dict - The dictionary for the loc file. The keys are the gen names and the values a list with the bands
+    :return d2: dict - The dictionary for the qua file. The keys are the positions and the values the numbers
+
     """
     d = {}
     d2 = {}
+    loc_line = ""
+    bands = []
+    start_reading = False
     with open(loc, "r") as loc_bes:
-        loc_line = ""
         for line in loc_bes:
             if " (a,b) ;" in line:
                 start_reading = True
                 if not loc_line == "":
-                    d.update({header: loc_line})
+                    for c in loc_line:
+                        if not c == " ":
+                            bands.append(c)
+                    d.update({header: bands})
+                    bands = []
+                    loc_line = ""
                 header = line.split(" (a,b) ;")[0]
             elif "individual names:" in line:
                 start_reading = False
-                d.update({header: loc_line})
+                for c in loc_line:
+                    if not c == " ":
+                        bands.append(c)
+                d.update({header: bands})
             elif start_reading:
-                loc_line += line
+                loc_line += line.strip("\n")
         loc_bes.close()
 
     with open(qua, "r") as qua_bes:
-
         for line in qua_bes:
             if line.startswith("1"):
                 start_reading = True
             elif line == "":
                 start_reading = False
-            elif start_reading:
+            if start_reading:
                 index, num = line.split("\t")
+                index = int(index)
+                num = num.replace("\n", "")
                 d2.update({index: num})
         qua_bes.close()
 
-    return
+    return d, d2
 
 
-def main():
+if __name__ == "__main__":
+    # print("/---------------------------------\\")
+    # print("|    QTL-opdracht assignment 2    |")
+    # print("|         Ontwikkeld door:        |")
+    # print("| Gabe van de Hoeven & Max Nollet |")
+    # print("\---------------------------------/\n")
 
     loc_bestand = "CvixLerC9.loc"
     qua_bestand = "CvixLerC9.qua"
 
     read_files(loc_bestand, qua_bestand)
-    return 0
-
-
-if __name__ == "__main__":
-    print("/---------------------------------\\")
-    print("|    QTL-opdracht assignment 2    |")
-    print("|         Ontwikkeld door:        |")
-    print("| Gabe van de Hoeven & Max Nollet |")
-    print("\---------------------------------/\n")
-    main()
